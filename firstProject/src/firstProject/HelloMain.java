61dcc1b67439b218683ccc1d31d3b999870068ad
Admin <Admin@DESKTOP-F51V0VD>
2021-06-23 14:34:20 +0900

    jj

---
+++ firstProject/src/firstProject/HelloMain.java
@@ -0,0 +1,11 @@
+
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		System.out.println("hello hyoin~");
+		
+		
+	}
+}
+
---
+++ firstProject/src/firstProject/NumberExaple.java
@@ -0,0 +1,11 @@
+package firstProject;
+
+public class NumberExaple {
+	 public static void main(String[] args) {
+byte math = 100; // -128~127
+byte eng= 90;
+
+byte sum = (byte) (math + eng);
+System.out.println(" 두 점수의 합 : " + sum );
+	 }
+}	 
